61d1905ad8681e3a27f072d19d4b8d6b21c932c8
Jocke <joakim.gumaelius@addinit.se>
2022-03-03 21:12:21 +0100

    sentry.io

---
+++ src/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.sentry_dsn = undefined;
+  window.__env.sentry_environment = "dev";
+  window.__env.liveresults_server = "https://liveresultat.orientering.se";
+}(this));
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,39 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from '@sentry/tracing';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const browserWindow = (window as { [key: string]: any }) || {};
+const browserWindowEnv = browserWindow['__env'] || {};
+const sentryDsn = browserWindowEnv.sentry_dsn;
+
+if(sentryDsn) {
+  const sentry_environment = browserWindowEnv.sentry_environment;
+
+  Sentry.init({
+    dsn: sentryDsn,
+    integrations: [
+      new BrowserTracing({
+        tracingOrigins: ["localhost"],
+        routingInstrumentation: Sentry.routingInstrumentation,
+      }),
+    ],
+    environment:sentry_environment,
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
